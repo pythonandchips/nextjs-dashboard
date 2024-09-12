ff15308e1662fab79e7257aaa8aa214277720fed
Colin Gemmell <pythonandchips@gmail.com>
2024-09-12 14:54:16 +0100

    fix up some naming

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,5 @@ import { Inter } from "next/font/google";
 import { Lustria } from "next/font/google";
 
 export const lustria = Lustria({ subsets: ["latin"], weight: "400" });
+export const lusitana = Lustria({ subsets: ["latin"], weight: "400" });
 export const inter = Inter({ subsets: ["latin"] });
