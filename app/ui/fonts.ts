8943375abf62fa49656549afa7bd3201b5392f25
Colin Gemmell <pythonandchips@gmail.com>
2024-09-12 14:45:37 +0100

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from "next/font/google";
+import { Lustria } from "next/font/google";
+
+export const lustria = Lustria({ subsets: ["latin"], weight: "400" });
+export const inter = Inter({ subsets: ["latin"] });
